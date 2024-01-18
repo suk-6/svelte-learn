78e91c7506b15e9859548e3de01c766ade79c018
suk-6 <me@suk.kr>
2024-01-19 00:18:46 +0900

    each + if-else

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,57 @@
 <script>
-	let auth = {
-		loggedIn: false,
-		loggedUser: null,
-	};
-	let userNameInput = "";
+	let todos = [
+		{
+			id: 1,
+			content: "할일1",
+			isDone: false,
+		},
+		{
+			id: 2,
+			content: "할일2",
+			isDone: false,
+		},
+		{
+			id: 3,
+			content: "할일3",
+			isDone: false,
+		},
+		{
+			id: 4,
+			content: "할일4",
+			isDone: false,
+		},
+	];
 
-	const loginHandle = () => {
-		auth.loggedIn = true;
-		if (userNameInput != "") {
-			auth.loggedUser = userNameInput;
+	const changeState = (todo) => {
+		for (let i = 0; i < todos.length; i++) {
+			if (todos[i].id == todo.id) {
+				todos[i].isDone = !todo.isDone;
+			}
 		}
-	};
-
-	const logoutHandle = () => {
-		auth.loggedIn = false;
 	};
 </script>
 
-{#if !auth.loggedIn}
-	<input type="text" bind:value={userNameInput} />
-	<button on:click={loginHandle}>Login</button>
-{:else}
-	<input type="text" bind:value={userNameInput} disabled />
-	<button on:click={logoutHandle}>Logout</button>
-{/if}
+<ul>
+	{#each todos as todo}
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		{#if todo.isDone}
+			<s>
+				<li
+					on:click={() => {
+						changeState(todo);
+					}}
+				>
+					{todo.content}
+				</li>
+			</s>
+		{:else}
+			<li
+				on:click={() => {
+					changeState(todo);
+				}}
+			>
+				{todo.content}
+			</li>
+		{/if}
+	{/each}
+</ul>
