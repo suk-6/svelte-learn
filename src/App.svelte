2e5ebaf248c6290c21a2f77a6f3d956a31ee0231
suk-6 <me@suk.kr>
2024-01-18 22:12:16 +0900

    버튼 클릭

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,11 @@
 <script>
-	export let name;
+	let count = 0;
+
+	const handleClick = () => {
+		count += 1;
+	};
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<button on:click={handleClick}>
+	클릭 수: {count}
+</button>
