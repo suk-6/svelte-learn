02ea9991f05ff5e2075c818e1885ff4e06bf0c17
suk-6 <me@suk.kr>
2024-01-20 01:21:55 +0900

    svelte/store

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,9 @@
 <script lang="ts">
-	let todos = [];
-
-	(() => {
-		for (let i = 1; i < 5; i++) {
-			todos.push({
-				id: i,
-				content: `할일 ${i}`,
-				isDone: false,
-			});
-		}
-	})();
-
-	const changeState = (todo: object) => {
-		for (let i = 0; i < todos.length; i++) {
-			if (todos[i].id === todo.id) {
-				todos[i].isDone = !todo.isDone;
-			}
-		}
-	};
+	import Increase from "./components/increase.svelte";
+	import Decrease from "./components/decrease.svelte";
+	import Result from "./components/result.svelte";
 </script>
 
-<ul>
-	{#each todos as todo}
-		<!-- svelte-ignore a11y-click-events-have-key-events -->
-		{#if todo.isDone}
-			<s>
-				<li
-					on:click={() => {
-						changeState(todo);
-					}}
-				>
-					{todo.content}
-				</li>
-			</s>
-		{:else}
-			<li
-				on:click={() => {
-					changeState(todo);
-				}}
-			>
-				{todo.content}
-			</li>
-		{/if}
-	{/each}
-</ul>
+<Increase />
+<Decrease />
+<Result />
---
+++ src/components/increase.svelte
@@ -0,0 +1,6 @@
+<script>
+	import { count } from "../stores";
+	const increaseHandle = () => ($count += 1);
+</script>
+
+<button on:click={increaseHandle}>증가</button>
