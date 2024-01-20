55812d247c9ca9b3d3eedec7c49dcb2d883325f1
suk-6 <me@suk.kr>
2024-01-20 12:59:24 +0900

    변동성 테스트

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+	import { count } from "./stores";
 	import Increase from "./components/increase.svelte";
 	import Decrease from "./components/decrease.svelte";
 	import Result from "./components/result.svelte";
+
+	$: if ($count < 0) {
+		alert("0 이하는 금지!");
+		$count = 0;
+	}
 </script>
 
 <Increase />
